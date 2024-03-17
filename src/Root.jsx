51de86f4b47f63bc29fcaad2e08c0d79d10854af
caoduc2003 <ducldche176120@fpt.edu.vn>
2024-03-18 00:33:27 +0700

    add templates

---
+++ src/Root.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./components/common/Header";
+import { Outlet } from "react-router-dom";
+
+const Root = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
+};
+
+export default Root;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,28 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import { RouterProvider, createBrowserRouter } from "react-router-dom"
-import Home from "./pages"
-import ModalsProvider from "./context/ModalsContext"
-import "bootstrap/dist/css/bootstrap.min.css"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import ModalsProvider from "./context/ModalsContext";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Root from "./Root";
+import Login from "./pages/auth/Login";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Home />,
-    },
-])
-const root = ReactDOM.createRoot(document.getElementById("root"))
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "login",
+        element: <Login />,
+      },
+    ],
+  },
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <ModalsProvider>
-            <RouterProvider router={router} />
-        </ModalsProvider>
-    </React.StrictMode>
-)
+  <React.StrictMode>
+    <ModalsProvider>
+      <RouterProvider router={router} />
+    </ModalsProvider>
+  </React.StrictMode>
+);
