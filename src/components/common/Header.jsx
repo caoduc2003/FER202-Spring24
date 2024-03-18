7137e3df7a5cdf5e961e090290112a24d822b6de
caoduc2003 <ducldche176120@fpt.edu.vn>
2024-03-18 10:14:33 +0700

    add navbar style

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import { Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <>
+      <Navbar expand="lg" className="bg-body-tertiary" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand>SUPER-CV</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+          <Navbar.Text>
+            Signed in as: {
+              
+            }
+          </Navbar.Text>
+        </Container>
+      </Navbar>
+    </>
+  );
 };
 
 export default Header;
